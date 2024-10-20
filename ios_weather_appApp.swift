7336ec4f43bdf519a0de3732387a91a27b8aed0d
cyburns <72774499+cyburns@users.noreply.github.com>
2024-10-19 21:13:39 -0400

    Initial commit

---
+++ ios_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weather_appApp.swift
+//  ios-weather-app
+//
+//  Created by Cyrus Burns on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
